576aaa61011f2e6e043aa53951a206151aff549b
german <gerferpin@gmail.com>
2025-04-29 13:35:08 +0200

    movimiento de carpetas a otra ruta

---
+++ Next-Stop/src/gps/GPS.java
@@ -0,0 +1,55 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class GPS {
+    // atributos
+    private String busId;
+    private LocalDateTime tiempobus;
+    private double latitud;
+    private double longitud;
+    private double velocidad;
+
+    // constructor
+    public GPS(String busId, LocalDateTime tiempobus, double latitud, double longitud, double velocidad) {
+        this.busId = busId;
+        this.tiempobus = tiempobus;
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.velocidad = velocidad;
+    }
+
+
+
+    // getters
+    public String getBusId() {
+        return busId;
+    }
+
+    public LocalDateTime getTiempobus() {
+        return tiempobus;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public String csv(){
+        // Formato de como se va a guardar en el csv
+        return String.format("%s,%s,%.5f,%.5f,%.2f",
+                busId,
+                tiempobus.toString(),
+                latitud,
+                longitud,
+                velocidad
+        );
+    }
+}
---
+++ Next-Stop/src/gps/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String archivo = "almacenamiento/datos_gps.csv"; // ruta de donde se guardan los datos
+        Generador.generador(archivo);
+    }
+}
