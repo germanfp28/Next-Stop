29f9250e7e01b47718cfd20b05fa886e48b2e054
german <gerferpin@gmail.com>
2025-05-12 00:26:13 +0200

    Actualizacion main

--- Next-Stop/src/gps/Main.java
+++ Next-Stop/src/gps/Main.java
@@ -1,8 +1,15 @@
 package gps;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         String archivo = "almacenamiento/datos_gps.csv"; // ruta de donde se guardan los datos
-        Generador.generador(archivo);
+        Generador.generador("almacenamiento/datos_gps.csv");
+        // lee y valida los datos del archivo csv
+        ArrayList<GPS> datos = Procesos.leerycomprobar(archivo);
+
+        // inicia el menu
+        Menu.menu(datos);
     }
 }
