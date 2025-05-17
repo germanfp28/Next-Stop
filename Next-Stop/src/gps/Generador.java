051ba0aebfeeba5e9b9ae0e00d99fbb111ceb3f8
german <gerferpin@gmail.com>
2025-05-17 12:48:35 +0200

    Nueva version de la clase que genera los datos

--- Next-Stop/src/gps/Generador.java
+++ Next-Stop/src/gps/Generador.java
@@ -7,49 +7,38 @@ import java.util.Random;
 import java.util.ArrayList;
 
 public class Generador {
-
+// esta clase genera los datos de los 3 autobuses y lo guarda en el archivo csv
     public static void generador(String archivo) {
         String[] busIds = {"Bus01", "Bus02", "Bus03"};
         LocalDateTime inicio = LocalDateTime.of(2025, 3, 25, 8, 0); // Hora de inicio
-
-        double latitud = 38.7169;   // Lisboa
-        double longitud = -9.1399;
-
         Random random = new Random();
-        ArrayList<GPS> todosLosDatos = new ArrayList<>();
 
-        // Generar datos para cada bus
-        for (String busId : busIds) {
-            LocalDateTime tiempo = inicio;
-            double lat = latitud;
-            double lon = longitud;
-
-            for (int i = 0; i < 60; i++) {
-                int velocidad = random.nextInt() * 50;
-
-                // Simular movimiento aleatorio
-                lat += (random.nextDouble() - 0.5) * 0.01;
-                lon += (random.nextDouble() - 0.5) * 0.01;
-
-                GPS g = new GPS(busId, tiempo, lon, lat, velocidad);
-                todosLosDatos.add(g);
-
-                tiempo = tiempo.plusMinutes(1);
-            }
-        }
-
-
-        // Guardar en CSV
         try (FileWriter fw = new FileWriter(archivo)) {
             fw.write("busId,tiempobus,latitud,longitud,velocidad\n");
 
-            for (GPS g : todosLosDatos) {
-                fw.write(g.csv() + "\n");
-            }
-            System.out.println("Datos GPS generados en: " + archivo);
 
+            // Generar datos para cada bus
+            for (String busId : busIds) {
+                LocalDateTime tiempo = inicio;
+                double latitud = 38.7169;   // Lisboa
+                double longitud = -9.1399;
+
+                for (int i = 0; i < 60; i++) {
+                    // Simular movimiento aleatorio
+                    latitud += (random.nextDouble() - 0.5) * 0.001;
+                    longitud += (random.nextDouble() - 0.5) * 0.001;
+                    int velocidad = random.nextInt(51); // valor aleatorio entre 0 y 50
+
+                    GPS g = new GPS(busId, tiempo, latitud, longitud, velocidad);
+                    fw.write(g.toCSV() + "\n");
+                    //incremento de tiempo en un 1minuto
+                    tiempo = tiempo.plusMinutes(1);
+                }
+            }
+            System.out.println("Datos generados exitosamente");
         } catch (IOException e) {
             System.out.println("Error al escribir el archivo CSV");
         }
+
     }
 }
